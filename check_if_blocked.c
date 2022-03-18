5b338fe844f38d2709284f2268fe17bffb4c1e28
ehelmine <ella.helminen@outlook.com>
2022-03-18 17:21:28 +0200

    added level 1 check

--- check_if_blocked.c
+++ check_if_blocked.c
@@ -24,15 +24,25 @@ void	check_if_player_is_blocked(char **board, int players[4][2])
 			(p_y + 4 < 19 && p_x + 7 < 34 && board[p_y + 4][p_x + 7] != ' ') &&
 			(p_y + 4 < 19 && p_x + 7 < 34 && board[p_y + 4][p_x + 7] != ' '))
 		{
-			winner = 2;
-			loser = 1;
-			if (y == 0 || y == 1)
+			if ((p_y - 4 > 1 && p_x - 7 > 1 && board[p_y - 4][p_x - 7] != '1')
+			&& (p_y - 4 > 1 && p_x > 1 && board[p_y - 4][p_x] != '1') && 
+			(p_y - 4 > 1 && p_x + 7 < 34 && board[p_y - 4][p_x + 7] != '1') &&
+			(p_y > 1 && p_x - 7 > 1 && board[p_y][p_x - 7] != '1') && 
+			(p_y > 1 && p_x + 7 < 34 && board[p_y][p_x + 7] != '1') &&
+			(p_y + 4 < 19 && p_x - 7 > 1 && board[p_y + 4][p_x - 7] != '1') && 
+			(p_y + 4 < 19 && p_x + 7 < 34 && board[p_y + 4][p_x + 7] != '1') &&
+			(p_y + 4 < 19 && p_x + 7 < 34 && board[p_y + 4][p_x + 7] != '1'))
 			{
-				winner = 1;
-				loser = 2;
+				winner = 2;
+				loser = 1;
+				if (y == 0 || y == 1)
+				{
+					winner = 1;
+					loser = 2;
+				}
+				printf("No moves left for player %i. Player number %i has won. Congrats!!\n", loser, winner);
+				exit (0);
 			}
-			printf("No moves left for player %i. Player number %i has won. Congrats!!\n", loser, winner);
-			exit (0);
 		}
 		y++;
 	}
