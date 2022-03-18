b297d714ad4e778b8276d2ed701f62ff77feb493
ehelmine <ella.helminen@outlook.com>
2022-03-19 01:06:00 +0200

    somewhat working, error management added

--- drawing.c
+++ drawing.c
@@ -1,35 +1,60 @@
 #include "santorini.h"
 
-void	our_error(char *str)
+void	check_if_someone_won(char **board, int players[4][2])
 {
-	printf("%s\n", str);
-	exit (1);
+	int y =0;
+	int x = 0;
+	int i = 0;
+	while (board[y] != NULL)
+	{
+		while (board[y][x] != '\0')
+		{
+			if (board[y][x] == '3')
+			{
+				i = 0;
+				while (i < 4)
+				{
+					if (players[i][0] == x && players[i][1] == y)
+					{
+						printf("Congrats! Somebody won!\n");
+						exit (0);
+					}
+					i++;
+				}
+			}
+			x++;
+		}
+		y++;
+		x = 0;
+	}
 }
 
 char	**get_board(void)
 {
-	int fd;
 	char **board;
-	size_t bufsize = 37;
-	char buf[38];
-
-	fd = open("board.txt", O_RDONLY);
-	if (!fd)
-		our_error("open error\n");
-	int i = 0;
 	board = (char **)malloc(sizeof(char *) * 22);
-	if (!board)
-		our_error("malloc error\n");
-	while (i < 21)
-	{
-		board[i] = (char *)malloc(sizeof(char) * 38);
-		if (!board[i])
-			our_error("malloc error\n");
-		if (read(fd, buf, 37) == -1)
-			our_error("read error\n");
-		strcpy(board[i++], buf);
-	}
-	board[i] = NULL;
+	board[0] = strdup("------------------------------------\n");
+	board[1] = strdup("|      |      |      |      |      |\n");
+	board[2] = strdup("|      |      |      |      |      |\n");
+	board[3] = strdup("|      |      |      |      |      |\n");
+	board[4] = strdup("|------|------|------|------|------|\n");
+	board[5] = strdup("|      |      |      |      |      |\n");
+	board[6] = strdup("|      |      |      |      |      |\n");
+	board[7] = strdup("|      |      |      |      |      |\n");
+	board[8] = strdup("|------|------|------|------|------|\n");
+	board[9] = strdup("|      |      |      |      |      |\n");
+	board[10] = strdup("|      |      |      |      |      |\n");
+	board[11] = strdup("|      |      |      |      |      |\n");
+	board[12] = strdup("|------|------|------|------|------|\n");
+	board[13] = strdup("|      |      |      |      |      |\n");
+	board[14] = strdup("|      |      |      |      |      |\n");
+	board[15] = strdup("|      |      |      |      |      |\n");
+	board[16] = strdup("|------|------|------|------|------|\n");
+	board[17] = strdup("|      |      |      |      |      |\n");
+	board[18] = strdup("|      |      |      |      |      |\n");
+	board[19] = strdup("|      |      |      |      |      |\n");
+	board[20] = strdup("------------------------------------\n");
+	board[21] = NULL;
 	return (board);
 }
 
@@ -37,45 +62,20 @@ void	drawing(player_info_t *p1, player_info_t *p2, char **board, int whose_turn_
 {
 	int	players[4][2];
 
-	players[0][0] = p1->w1row;
-	players[0][1] = p1->w1col;
-	players[1][0] = p1->w2row;
-	players[1][1] = p1->w2col;
-	players[2][0] = p2->w1row;
-	players[2][1] = p2->w1col;
-	players[3][0] = p2->w2row;
-	players[3][1] = p2->w2col;
+	players[0][0] = p1->w1col;
+	players[0][1] = p1->w1row;
+	players[1][0] = p1->w2col;
+	players[1][1] = p1->w2row;
+	players[2][0] = p2->w1col;
+	players[2][1] = p2->w1row;
+	players[3][0] = p2->w2col;
+	players[3][1] = p2->w2row;
 	int i = 0;
 	while (i < 4)
 		change_player_coordinates_for_board(players[i++]);
-	printf("%i\n", players[0][0]);
-	printf("%i\n", players[0][1]);
-	printf("%i\n", players[1][0]);
-	printf("%i\n", players[1][1]);
-	printf("%i\n", players[2][0]);
-	printf("%i\n", players[2][1]);
-	printf("%i\n", players[3][0]);
-	printf("%i\n", players[3][1]);
-/*	n_players = (int **)malloc(sizeof(int *) * 4);
-	i = 0;
-	while (i < 4)
-		n_players[i++] = (int *)malloc(sizeof(int) * 2);
-	n_players[0][0] = players[0][0];
-	n_players[0][1] = players[0][1];
-	n_players[1][0] = players[1][0];
-	n_players[1][1] = players[1][1];
-	n_players[2][0] = players[2][0];
-	n_players[2][1] = players[2][1];
-	n_players[3][0] = players[3][0];
-	n_players[3][1] = players[3][1];*/
 	print_board(board, players);
-//	if (players[3][1] != -1)
-//		check_if_player_is_blocked(board, players, whose_turn_next);
-/*	i = 0;
-	while (i < 4)
-	{
-		free(n_players[i]);
-		n_players[i++] = NULL;
-	}
-	free(n_players);*/
+	if (players[3][1] != 1)
+		check_if_someone_won(board, players);
+	if (players[3][1] != -1)
+		check_if_player_is_blocked(board, players, whose_turn_next);
 }
--- main.c
+++ main.c
@@ -1,21 +1,53 @@
 #include "santorini.h"
-/*
-void print_board(int **board)
+
+void	compare_block_to_players(player_info_t *player, player_info_t *another, int *block)
 {
-	for (int row = 0; row < 5; row++)
+	if (player->w1col == block[0] && player->w1row == block[1])
 	{
-		for (int col = 0; col < 5; col++)
-		{
-			if (board[row][col] == 6)
-				printf("%s\t", "A");
-			else if (board[row][col] == 7)
-				printf("%s\t", "X");
-			else
-				printf("%d\t", board[row][col]);
-		}
-		printf("\n");
+		printf("Don't try to build under player.\n");
+		exit (1);
 	}
-}*/
+	if (player->w2col == block[0] && player->w2row == block[1])
+	{
+		printf("Don't try to build under player.\n");
+		exit (1);
+	}
+	if (another->w1col == block[0] && another->w1row == block[1])
+	{
+		printf("Don't try to build under player.\n");
+		exit (1);
+	}
+	if (another->w2col == block[0] && another->w2row == block[1])
+	{
+		printf("Don't try to build under player.\n");
+		exit (1);
+	}
+}
+
+void	compare_players(player_info_t *player, player_info_t *another)
+{
+	if (player->w1col == player->w2col && player->w1row == player->w2row)
+	{
+		printf("Don't put players in the same place.\n");
+		exit (1);
+	}
+	if (another->w1col == another->w2col && another->w1row == another->w2row)
+	{
+		printf("Don't put players in the same place.\n");
+		exit (1);
+	}
+	if (player->w1col == another->w2col && player->w1row == another->w2row)
+	{
+		printf("Don't put players in the same place.\n");
+		exit (1);
+	}
+	if (another->w1col == player->w2col && another->w1row == player->w2row)
+	{
+		printf("Don't put players in the same place.\n");
+		exit (1);
+	}
+}
+
 int isdirection(char *str)
 {
 	str[0] = toupper(str[0]);
@@ -96,16 +128,12 @@ int    *is_valid_move(int dir, player_info_t *player, int worker)
 		wrow = player->w2row;
 		wcol = player->w2col;
 	}
-
 	int *target;
 	target = (int *)malloc(sizeof(int) * 2);
 	target[0] = offsets[dir][0] + wcol;
 	target[1] = offsets[dir][1] + wrow;	
 	if (target[0] < 0 || target[0] > 4 || target[1] < 0 || target[1] > 4)
-	{
-		printf("not good value\n");
 		return (NULL);
-	}
 	return (target);
 }
 
@@ -118,6 +146,7 @@ int get_command(player_info_t *player, char **new_board, player_info_t *another,
 	int bdir;
 	int *block;
 
+	buffer = NULL;
 	printf("Choose worker: ");
 	worker = get_input(1, 2) + 1;
 	printf("\nChoose direction to move: ");
@@ -134,18 +163,26 @@ int get_command(player_info_t *player, char **new_board, player_info_t *another,
 				printf("Invalid direction\n");
 		} while(mvdir < 0);
 	block = is_valid_move(mvdir, player, worker);
+	if (block == NULL)
+	{
+		printf("Wrong direction. Please start again\n");
+		exit (1);
+	}
+	if (worker == 1)
+	{
+		player->w1col = block[0];
+		player->w1row = block[1];
+	}
+	else if (worker == 2)
+	{
+		player->w2col = block[0];
+		player->w2row = block[1];
+	}
+	compare_players(player, another);
 	if (who == 1)
-	{
-		player->w1row = block[0];
-		player->w1col = block[1];
 		drawing(player, another, new_board, 0);
-	}
 	else if (who == 2)
-	{
-		player->w2row = block[0];
-		player->w2col = block[1];
 		drawing(another, player, new_board, 0);
-	}
 	printf("\nChoose direction to build: ");
 	
 	build:
@@ -160,10 +197,14 @@ int get_command(player_info_t *player, char **new_board, player_info_t *another,
 			    printf("Invalid direction\n");
 		} while(bdir < 0);
 
-	block = is_valid_move(mvdir, player, worker);
-	printf("1 x %i y %i\n", block[0], block[1]);
+	block = is_valid_move(bdir, player, worker);
+	if (block == NULL)
+	{
+		printf("Wrong direction. Please start again\n");
+		exit (1);
+	}
+	compare_block_to_players(player, another, block);
 	change_block_coordinates(block);
-	printf("2 x %i y %i\n", block[0], block[1]);
 	add_level_letters_to_board(new_board, block);
 	return (0);
 }
@@ -179,9 +220,9 @@ char **init_players(player_info_t *p1, player_info_t *p2, int **board)
 //	return ;
 	do {
 		printf("Worker 1 starting row: ");
-		wrow = get_input(1, 5);
-		printf("Worker 1 starting col: ");
 		wcol = get_input(1, 5);
+		printf("Worker 1 starting col: ");
+		wrow = get_input(1, 5);
 	} while(!isfree_coord(p1, wrow, wcol, board));
 
 	p1->w1row = wrow;
@@ -193,9 +234,9 @@ char **init_players(player_info_t *p1, player_info_t *p2, int **board)
 	printf("\n");
 	do {
 		printf("Worker 2 starting row: ");
-		wrow = get_input(1, 5);
-		printf("Worker 2 starting col: ");
 		wcol = get_input(1, 5);
+		printf("Worker 2 starting col: ");
+		wrow = get_input(1, 5);
 	} while(!isfree_coord(p1, wrow, wcol, board));
 
 	p1->w2row = wrow;
@@ -207,9 +248,9 @@ char **init_players(player_info_t *p1, player_info_t *p2, int **board)
 
 	do {
 		printf("Worker 1 starting row: ");
-		wrow = get_input(1, 5);
-		printf("Worker 1 starting col: ");
 		wcol = get_input(1, 5);
+		printf("Worker 1 starting col: ");
+		wrow = get_input(1, 5);
 	} while(!isfree_coord(p2, wrow, wcol, board));
 
 	p2->w1row = wrow;
@@ -220,9 +261,9 @@ char **init_players(player_info_t *p1, player_info_t *p2, int **board)
 	printf("\n");
 	do {
 		printf("Worker 2 starting row: ");
-		wrow = get_input(1, 5);
-		printf("Worker 2 starting col: ");
 		wcol = get_input(1, 5);
+		printf("Worker 2 starting col: ");
+		wrow = get_input(1, 5);
 	} while(!isfree_coord(p2, wrow, wcol, board));
 
 	p2->w2row = wrow;
@@ -247,6 +288,7 @@ void init_board(int **board)
 int main(void)
 {
 	int whose_turn_next = 1;
+	int temp;
 	int **board = (int **)malloc(sizeof(int) * 7);
 	player_info_t *p1 = (player_info_t *)malloc(sizeof(player_info_t));
 	player_info_t *p2 = (player_info_t *)malloc(sizeof(player_info_t));
@@ -265,9 +307,9 @@ int main(void)
 	while (1)
 	{
 		get_command(p1, new_board, p2, 1);
-		drawing(p1, p2, new_board, 0);
+		drawing(p1, p2, new_board, 2);
 		get_command(p2, new_board, p1, 2);
-		drawing(p1, p2, new_board, 0);
+		drawing(p1, p2, new_board, 1);
 	}
 
 	// free(p1);
